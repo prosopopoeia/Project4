c984655a047ae7d4d0fd06c1f746d22a2c590278
Solomon Grundy <ngwtt@yahoo.com>
2020-10-14 19:45:55 -0600

    migrations, static files, some template stuff

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Post, Following, Likes
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Following)
+admin.site.register(Likes)
--- network/models.py
+++ network/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    number_followers=models.IntegerField(default=0)
+    
+class Post(models.Model):
+    author=models.ForeignKey("User", on_delete=models.CASCADE)
+    subect=models.CharField(max_length=255)
+    body=models.TextField(blank=True)
+    timestamp=models.DateTimeField(max_length=255)
+    
+class Following(models.Model):
+    follower=models.ForeignKey("User", on_delete=models.CASCADE, related_name="networkFollowingUser")
+    followed=models.ForeignKey("User", on_delete=models.CASCADE, related_name="networkFollowedUser")
+
+class Likes(models.Model):
+    liker=models.ForeignKey("User", on_delete=models.CASCADE)
+    liked=models.ForeignKey("Post", on_delete=models.CASCADE)
+    
